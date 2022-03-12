0fb63fdd6188b836560d394a6b99a8feb9a7d4bc
renesiodlaczek <r.siodlaczek@web.de>
2022-03-12 12:29:50 +0000

    added some notes

--- Scripts/1_firstscript.R
+++ Scripts/1_firstscript.R
@@ -9,3 +9,24 @@ plot(mpg$cyl, mpg$hwy)
 # test comment
 
 #git config --global user.name "Jane Doe"
+system('git config --global user.name "John Doe"')
+
+###### Access Token instead of Password ########
+#https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
+#################
+
+## Option 1: Write to a git config file
+
+# Bare minimum code profile.
+write_git_identity = function(name = "John Doe",
+                              email = "johndoe@example.com", 
+                              file = "~/.gitconfig") {
+  sink(file = file)
+  cat("[user]\n")
+  cat("  name = ",name,"\n", sep="")
+  cat("  email = ",email, sep="")
+  sink() 
+}
+
+# Supply credentials
+write_git_identity("FirstName LastName", "netid@illinois.edu")
